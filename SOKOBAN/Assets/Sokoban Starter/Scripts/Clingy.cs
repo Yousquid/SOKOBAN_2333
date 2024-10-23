287e3c3dc964f9cc24ea947bc87a5d86b060184f
Yousquid <ly2456@nyu.edu>
2024-10-24 02:57:37 +0800

    Pulling_bug_fixed

--- SOKOBAN/Assets/Sokoban Starter/Scripts/Clingy.cs
+++ SOKOBAN/Assets/Sokoban Starter/Scripts/Clingy.cs
@@ -17,150 +17,101 @@ public class Clingy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        print(pull);
         PullBlcok();
     }
-   
+
     void PullBlcok()
     {
-         GameObject clingy_object = gameManager_New.box_object["clingy"];
-
-         Box_data clingy_data = clingy_object.GetComponent<Box_data>();
-
+        GameObject clingy_object = gameManager_New.box_object["clingy"];
+        Box_data clingy_data = clingy_object.GetComponent<Box_data>();
         Vector2Int CurrentPosition = clingy_data.gridObject.gridPosition;
 
-         GameObject player_ob = gameManager_New.box_object["player"];
-
-         Box_data player_data = player_ob.GetComponent<Box_data>();
-
-         GameObject sticky_ob = gameManager_New.box_object["sticky"];
-
-         Box_data sticky_data = sticky_ob.GetComponent<Box_data>();
+        GameObject player_ob = gameManager_New.box_object["player"];
+        Box_data player_data = player_ob.GetComponent<Box_data>();
 
         int Player_Xposition = player_data.gridObject.gridPosition.x;
-
         int Player_Yposition = player_data.gridObject.gridPosition.y;
 
-        int Sticky_Xposition = sticky_data.gridObject.gridPosition.x;
-
-        int Sticky_Yposition = sticky_data.gridObject.gridPosition.y;
-
         int Clingy_Xposition = clingy_data.gridObject.gridPosition.x;
-
         int Clingy_Yposition = clingy_data.gridObject.gridPosition.y;
 
+        // Horizontal Pull Logic
         if (Clingy_Yposition == Player_Yposition)
         {
-
-            if (Clingy_Yposition == Player_Yposition && Clingy_Xposition - Player_Xposition == 1)
+            if (Clingy_Xposition - Player_Xposition == 1)
             {
                 pull = true;
-                timer = 0;
             }
-            else { timer++; }
-            if (timer >= 10)
-            {
-                pull = false;
-            }
+
             if (pull && Clingy_Xposition - Player_Xposition == 2)
             {
                 clingy_data.gridObject.gridPosition.x -= 1;
 
                 Vector2Int NewPosition = clingy_data.gridObject.gridPosition;
+                gameManager_New.boxDataDictionary.Remove(CurrentPosition);
+                gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
 
-                if (gameManager_New.boxDataDictionary.ContainsKey(CurrentPosition))
-                {
-                    gameManager_New.boxDataDictionary.Remove(CurrentPosition);
-                }
-
-                if (!gameManager_New.boxDataDictionary.ContainsKey(NewPosition))
-                {
-                    gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
-                }
                 pull = false;
             }
-
-
-
-
-
-
-            if (Clingy_Yposition == Player_Yposition && Clingy_Xposition - Player_Xposition == -1)
+            else if (Clingy_Xposition - Player_Xposition == -1)
             {
                 pull = true;
             }
-            
+
             if (pull && Clingy_Xposition - Player_Xposition == -2)
             {
                 clingy_data.gridObject.gridPosition.x += 1;
 
                 Vector2Int NewPosition = clingy_data.gridObject.gridPosition;
+                gameManager_New.boxDataDictionary.Remove(CurrentPosition);
+                gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
 
-                if (gameManager_New.boxDataDictionary.ContainsKey(CurrentPosition))
-                {
-                    gameManager_New.boxDataDictionary.Remove(CurrentPosition);
-                }
-
-                if (!gameManager_New.boxDataDictionary.ContainsKey(NewPosition))
-                {
-                    gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
-                }
                 pull = false;
             }
         }
 
-        if (Clingy_Xposition == Player_Xposition)
+        // Vertical Pull Logic
+        else if (Clingy_Xposition == Player_Xposition)
         {
-
-            if (Clingy_Xposition == Player_Xposition && Clingy_Yposition - Player_Yposition == 1)
+            if (Clingy_Yposition - Player_Yposition == 1)
             {
                 pull = true;
             }
-           
+
             if (pull && Clingy_Yposition - Player_Yposition == 2)
             {
                 clingy_data.gridObject.gridPosition.y -= 1;
 
                 Vector2Int NewPosition = clingy_data.gridObject.gridPosition;
+                gameManager_New.boxDataDictionary.Remove(CurrentPosition);
+                gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
 
-                if (gameManager_New.boxDataDictionary.ContainsKey(CurrentPosition))
-                {
-                    gameManager_New.boxDataDictionary.Remove(CurrentPosition);
-                }
-
-                if (!gameManager_New.boxDataDictionary.ContainsKey(NewPosition))
-                {
-                    gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
-                }
                 pull = false;
             }
-
-
-
-
-
-            if (Clingy_Xposition == Player_Xposition && Clingy_Yposition - Player_Yposition == -1)
+            else if (Clingy_Yposition - Player_Yposition == -1)
             {
                 pull = true;
             }
-          
+
             if (pull && Clingy_Yposition - Player_Yposition == -2)
             {
                 clingy_data.gridObject.gridPosition.y += 1;
 
                 Vector2Int NewPosition = clingy_data.gridObject.gridPosition;
+                gameManager_New.boxDataDictionary.Remove(CurrentPosition);
+                gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
 
-                if (gameManager_New.boxDataDictionary.ContainsKey(CurrentPosition))
-                {
-                    gameManager_New.boxDataDictionary.Remove(CurrentPosition);
-                }
-
-                if (!gameManager_New.boxDataDictionary.ContainsKey(NewPosition))
-                {
-                    gameManager_New.boxDataDictionary.Add(NewPosition, clingy_data);
-                }
                 pull = false;
             }
         }
+        else
+        {
+            // If the block is neither horizontally nor vertically aligned with the player, reset pull
+            pull = false;
+        }
     }
+
+
+
 }
